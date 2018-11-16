648b0e4c253bd071a5a9162e03a1cd219b308375
evan-sauers <ebsauers@hotmail.com>
2018-11-15 22:03:34 -0500

    Created Dashboard Page
    - Created dashboard.php page
    - Created styles.css
    - Uploaded background image and IWU logo image
    - Began creating the dashboard page, such as columns and navigation
    - Utilizing Bootstrap 4

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,5 @@
 <?php
-    include('session.php');
+    //include('session.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,29 +10,62 @@
         <link rel="stylesheet" href="styles/styles.css">
         <link href="https://fonts.googleapis.com/css?family=ABeeZee" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+        <!-- Bootstrap jQuery -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
     <body>        
+        <!-- Navigation Bar -->
+        <nav class="navbar bg-dark navbar-dark">
+            <a class="navbar-brand" href="#"><h2><img src="images/iwu-logo.gif" id="logo"> Roommate Finder</h2></a>
+
+            <!-- Toggle "Hamburger" Button -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Navigation Bar links -->
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Logout</a>
+                    </li> 
+                </ul>
+            </div> 
+        </nav>
+
         <div class="container-fluid">
-            <!-- Title -->
             <div class="row">
-              <div class="col-lg-6">
-                  <h1>[INSERT LOGO HERE]]</h1>
-              </div>
-                <div class="col-lg-6">
-                    <div id="rightNav">
-                        <h4>Welcome, <?php echo $session; ?></h4>&nbsp;&nbsp;
-                        <h4><a href="logout.php">Logout</a></h4>
-                    </div>
+                <!-- Process Section -->
+                <div class="col-lg-3">
+                    <h2><u>Your Process</u></h2>
+                    <ol>
+                        <li>Take The Survey</li>
+                        <li>Find A Roommate</li>
+                        <li>Choose Your Housing</li>
+                    </ol>
                 </div>
-            </div>
-            
-            <!-- Footer -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="footer">Created by Cynthia Carter, Evan Sauers, and Josh Schoonmaker.</p>
+
+                <!-- Welcome Section -->
+                <div class="col-lg-8">
+                    <div class="row">
+                        <h1>Welcome</h1>
+                    </div>
+                    <div class="row">
+                        <button type="submit">Take the Survey</button>
+                    </div>
                 </div>
             </div>
         </div>
