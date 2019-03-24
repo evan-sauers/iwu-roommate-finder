9ee93fdb152748bc4b31fcda06f73eabaed533fc
evan-sauers <ebsauers@hotmail.com>
2019-03-24 16:24:46 -0400

    Added sex option
    - Added biological sex option to profile page
    - Made redirection to saved profile page once created profile is completed

--- writedata.js
+++ writedata.js
@@ -8,18 +8,18 @@ function submitBtn() {
     var first = $("#first").val();
     var last = $("#last").val();
     var list = $("#list").val();
+    var sex = $("#sex").val();
     var city = $("#city").val();
     var inputState = $("#inputState").val();
     var comment = $("#comment").val();
     var email = $("#email").val();
     
-    
-    var userID = user.email.replace(/[^a-zA-Z ]/g, "");
-        
+    var userID = user.email.replace(/[^a-zA-Z ]/g, "");  
     
     ref.set({
         "first" : first,
         "last" : last,
+        "sex" : sex,
         "list" : list,
         "city" : city,
         "inputState" : inputState,
@@ -27,4 +27,6 @@ function submitBtn() {
         "email" : email,
         "userID" : userID
     });
+    
+    window.location.replace("profileSave.html");
 }
