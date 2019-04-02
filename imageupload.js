e6d4dedf1d183c9dc274e685be3dd5d716697885
Joshua Schoonmaker <44808356+joshua-schoonmaker@users.noreply.github.com>
2019-04-02 09:04:56 -0400

    File Validated

--- imageupload.js
+++ imageupload.js
@@ -3,7 +3,7 @@ var progress = document.getElementById('progress');
 var imageUpload = document.getElementById('imageUpload');
 
 // Update progress bar and upload image to storage
-imageUpload.addEventListener('change', function(imageFile){
+imageUpload.addEventListener('change', function (imageFile) {
     // Create file and upload
     var file = imageFile.target.files[0];
     var storageRef = firebase.storage().ref('images/' + userState);
@@ -12,8 +12,8 @@ imageUpload.addEventListener('change', function(imageFile){
     // Update progress bar
     task.on('state_changed', 
         function bar(snapshot) {
-        var percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        progress.value = percent;
-    }
-    );
+            var percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            progress.value = percent;
+        }
+        );
 });
